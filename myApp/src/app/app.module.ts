e010d1703a69b641f9a18a5cfaad47c6e6dc86e4
Wojciech Paluch <wojciech.paluch@o2.pl>
2018-03-05 11:44:55 +0100

    added ibeacon

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { IBeacon } from '@ionic-native/ibeacon'
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -33,6 +33,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   providers: [
     StatusBar,
+    IBeacon,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AboutPage } from '../about/about';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {AboutPage} from '../about/about';
+import {IBeacon} from '@ionic-native/ibeacon';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,48 @@ import { AboutPage } from '../about/about';
 })
 export class HomePage {
 
-  ChangePage()
-  {
+  delegate:any;
+  region:any;
+
+  ChangePage() {
     this.navCtrl.setRoot(AboutPage);
   }
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController, private ibeacon: IBeacon) {
 
   }
 
+  initScanner() {
+
+    this.delegate.didRangeBeaconsInRegion()
+      .subscribe(
+        data => console.log('didRangeBeaconsInRegion: ', data),
+        error => console.error()
+      );
+    this.delegate.didStartMonitoringForRegion()
+      .subscribe(
+        data => console.log('didStartMonitoringForRegion: ', data),
+        error => console.error()
+      );
+    this.delegate.didEnterRegion()
+      .subscribe(
+        data => {
+          console.log('didEnterRegion: ', data);
+        }
+      );
+
+    let beaconRegion = this.ibeacon.BeaconRegion('Kontakt', 'f7826da6-4fa2-4e98-8024-bc5b71e0893e');
+
+    this.ibeacon.startMonitoringForRegion(beaconRegion)
+      .then(
+        () => console.log('Native layer recieved the request to monitoring'),
+        error => console.error('Native layer failed to begin monitoring: ', error)
+      );
+
+
+  }
 }
+
+
+
+
